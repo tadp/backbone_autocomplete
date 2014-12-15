3817acff2e530e99dc64728c00680d9404e67615
Tad Park <june@tutormentor.org>
2014-12-14 16:29:13 -0800

    adds rails 4 and backbone, underscorejs

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
-gem 'jbuilder', '~> 2.0'
+gem 'active_model_serializers', '~> 0.8.1'   
 
 gem 'spring',        group: :development
 # Use ActiveModel has_secure_password
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '5bf9981cff5f1f94129ca1049a266083744cf9cd70801891a92c584166ead4a24a051b0968034a60525bd08bfb145d5b3b391ca37350f07e290872cab09d54e1'
